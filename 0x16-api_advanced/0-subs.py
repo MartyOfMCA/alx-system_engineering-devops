8d40bb19ddb54c83d89d9d18d4c05c7da5720f6d
Martin <martyofmca@gmail.com>
2023-11-07 18:29:10 +0000

    fetch total subscribers for a subreddit - 0x16

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Define a function that queries the
+Reddit API.
+"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    Query the Reddit API and return the number of
+    subscribers for a given subreddit.
+
+    Parameters
+    subreddit : string
+        The subreddit to query
+
+    Return
+        The total number of subscribers for the
+        given subreddit. 0 is returned in the
+        case of an invalid subreddit.
+    """
+    subs = 0
+    results = None
+
+    with get(f"https://www.reddit.com/r/{subreddit}/top.json") as response:
+        if (response.status_code == 200):
+            res = response.json()
+            subs = res["data"]["children"][0]["data"]["subreddit_subscribers"]
+
+    return (subs)
