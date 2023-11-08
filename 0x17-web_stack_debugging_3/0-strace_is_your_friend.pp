c942b6f1374612a79a5d5432df4d2355ecbeb392
Martin <martyofmca@gmail.com>
2023-11-08 19:01:34 +0000

    fix typo in WordPress settings file - 0x17

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Correct typo in a file name extension from the wp-settings.php file
+
+exec { 'correct typo in WordPress settings file':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    => '/bin'
+}
